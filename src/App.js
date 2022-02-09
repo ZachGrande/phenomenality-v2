044b6ef30aaf77ea25b2459fe839797c313ecad5
ZachGrande <zach.grande23@gmail.com>
2022-02-08 19:18:00 -0800

    Re-initialized React and Firebase

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. This is a test.
+          Edit <code>src/App.js</code> and save to reload. Test!
         </p>
         <a
           className="App-link"
@@ -17,6 +17,7 @@ function App() {
         >
           Learn React
         </a>
+        <img src="ironman.jpg" alt="iron man"></img>
       </header>
     </div>
   );
