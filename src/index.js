f076e8ce90f9a27876c3d949b018a334380fa262
ZachGrande <zach.grande23@gmail.com>
2022-02-11 15:08:27 -0800

    Added Firebase SDK signature

--- src/index.js
+++ src/index.js
@@ -5,6 +5,31 @@ import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA_6VtPAZLUSF6828G0peEoERJdDDZ8fy0",
+  authDomain: "phenomenality-bdf65.firebaseapp.com",
+  databaseURL: "https://phenomenality-bdf65-default-rtdb.firebaseio.com",
+  projectId: "phenomenality-bdf65",
+  storageBucket: "phenomenality-bdf65.appspot.com",
+  messagingSenderId: "47065258935",
+  appId: "1:47065258935:web:e1c40865d25e46a80ce632",
+  measurementId: "G-HP9BTNZ836"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const database = getDatabase(app);
+
 ReactDOM.render(
   <React.StrictMode>
     <Router>
