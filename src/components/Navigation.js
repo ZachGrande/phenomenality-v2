fda9f73a418f2e320ed36010a7437457477bc25c
ZachGrande <zach.grande23@gmail.com>
2022-02-25 22:01:40 -0800

    Removed commented code in Navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -9,51 +9,39 @@ function Navigation() {
           <p>Strengthen Your Mentality</p>
           <li>
             <Link to="/">
-              {/* <a className="">Home</a> */}
               Home
             </Link>
           </li>
           <li>
             <Link to="/bank">
-              {/* <a>Your Bank</a> */}
               Your Bank
             </Link>
           </li>
           <li>
             <Link to="questions">
-              {/* <a>Q&#38;A</a> */}
               Q&#38;A
             </Link>
           </li>
           <li>
             <Link to="quiz">
-              {/* <a>Imposter Quiz</a> */}
               Imposter Quiz
             </Link>
           </li>
           <li>
             <Link to="more-info">
-              {/* <a>More Imposter Information</a> */}
               More Imposter Information
             </Link>
           </li>
           <li>
             <Link to="/about">
-              {/* <a className="">About Us</a> */}
               About Us
             </Link>
           </li>
           <li>
             <Link to="/authentication">
-              {/* <a className="">Sign In</a> */}
               Sign In
             </Link>
           </li>
-          {/* <li>
-            <Link to="/bank-new">
-              New bank
-            </Link>
-          </li> */}
           <p>Profile block</p>
         </ul>
       </div>
