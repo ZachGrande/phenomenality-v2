98ef41d774129645395b39664f3e2dda0215738c
ZachGrande <zach.grande23@gmail.com>
2022-02-27 19:57:18 -0800

    Moved Form to new file

--- src/components/Bank.js
+++ src/components/Bank.js
@@ -4,133 +4,12 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import app from '../config';
 import { getAuth } from 'firebase/auth';
 import { getDatabase, ref, onValue, update } from 'firebase/database';
+import Form from './Form.js';
 
 import 'firebase/auth';
 import 'firebase/database';
 
 import CardList from './Card.js';
-import { map } from '@firebase/util';
-
-function EntryForm(props) {
-
-  const items = props.items;
-  const setItems = props.setItems;
-  const database = props.database;
-  const user = props.user;
-  const setFilter = props.setFilter;
-
-  const [accomplishment, setAccomplishment] = useState("");
-  const [status, setStatus] = useState("success");
-  const [tags, setTags] = useState([]);
-
-  const addNewAccomplishment = async (event) => {
-    event.preventDefault();
-    let thisAccomplishment = {
-      complete: true,
-      description: accomplishment,
-      id: items.length + 1,
-      key: items.length + "",
-      status: status,
-      tags: tags
-    }
-    let newItems = items.push(thisAccomplishment);
-    newItems = map((currentItem, index = 0, newItems) => {
-      currentItem.id = index + 1;
-      currentItem.key = index + "";
-      index = index + 1;
-      return currentItem;
-    })
-    setItems(newItems);
-    setAccomplishment("");
-    update(ref(database, 'users/' + user.uid), {
-        data: items
-    });
-  }
-
-  const editTag = value => {
-    let newTags = tags;
-    if (!tags.includes(value)) {
-      newTags.push(value)
-    } else {
-      let index = newTags.indexOf(value);
-      if (index > -1) {
-        newTags.splice(index, 1);
-      }
-    }
-    setTags(newTags);
-  }
-
-  return (
-     <div>
-       <p>Bank Page</p>
-       <form>
-       <h4>What's something you're proud of?</h4>
-       <p><em>This only works if you are already logged in.</em></p>
-       <input
-         placeholder="Today I was able to..."
-         value={accomplishment}
-         onChange={(event) => {
-           setAccomplishment(event.target.value);
-         }}
-       />
-       <br></br>
-       <input
-         type="radio"
-         value="success"
-         name="status"
-         defaultChecked={status === "success"}
-         onChange={e => setStatus(e.currentTarget.value)}
-       /> Success
-       <br></br>
-       <input
-         type="radio"
-         value="question-unanswered"
-         name="status"
-         onChange={e => setStatus(e.currentTarget.value)}
-       /> Question (Unanswered)
-       <br></br>
-       <p><u>Tags</u></p>
-       <input
-         type="checkbox"
-         value="technical"
-         name="tag"
-         onChange={e => editTag(e.currentTarget.value)}
-       /> Technical
-       <br></br>
-       <input
-         type="checkbox"
-         value="soft skills"
-         name="tag"
-         onChange={e => editTag(e.currentTarget.value)}
-       /> Soft Skills
-       <br></br>
-       <br></br>
-       <button onClick={addNewAccomplishment}>Add accomplishment</button>
-       </form>
-       <br></br>
-       <h4>Want to filter by a specific tag?</h4>
-       <input
-         type="radio"
-         value="none"
-         name="filter"
-         defaultChecked={true}
-         onChange={e => setFilter(e.currentTarget.value)}
-       /> None
-       <input
-         type="radio"
-         value="technical"
-         name="filter"
-         onChange={e => setFilter(e.currentTarget.value)}
-       /> Technical
-       <input
-         type="radio"
-         value="soft skills"
-         name="filter"
-         onChange={e => setFilter(e.currentTarget.value)}
-       /> Soft Skills
-     </div>
-   )
- }
 
 function Bank(props) {
 
@@ -334,7 +213,7 @@ function Bank(props) {
   if (items.length > 0) {
     return (
       <div>
-        <EntryForm items={items}
+        <Form items={items}
                    setItems={setItems}
                    database={database}
                    user={user}
@@ -351,7 +230,7 @@ function Bank(props) {
     return (
       <div>
         <p>You have not added to your credibility bank!</p>
-        <EntryForm items={items}
+        <Form items={items}
                    setItems={setItems}
                    database={database}
                    user={user}
