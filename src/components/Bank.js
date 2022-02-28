10874012834918fd7ba28263c1a8ce88c8037144
ZachGrande <zach.grande23@gmail.com>
2022-02-27 19:59:02 -0800

    Removed commented Form code from Bank

--- src/components/Bank.js
+++ src/components/Bank.js
@@ -11,7 +11,7 @@ import 'firebase/database';
 
 import CardList from './Card.js';
 
-function Bank(props) {
+function Bank() {
 
   const auth = getAuth(app);
   const database = getDatabase(app);
@@ -21,20 +21,7 @@ function Bank(props) {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
-  // const [accomplishment, setAccomplishment] = useState("");
-  // const [status, setStatus] = useState("success");
-  // const [tags, setTags] = useState([]);
-
   const [filter, setFilter] = useState("none");
-  
-  // console.log("Current state:", items);
-
-  // console.log("Loading", loading);
-
-  // console.log("user", user);
-
-  // console.log("Tags", tags);
-  // console.log("Filtering by", filter);
 
   useEffect(() => {
     if (user !== null) {
@@ -75,30 +62,6 @@ function Bank(props) {
     )
   }
 
-  /*const addNewAccomplishment = async (event) => {
-    event.preventDefault();
-    let thisAccomplishment = {
-      complete: true,
-      description: accomplishment,
-      id: items.length + 1,
-      key: items.length + "",
-      status: status,
-      tags: tags
-    }
-    let newItems = items.push(thisAccomplishment);
-    newItems = map((currentItem, index = 0, newItems) => {
-      currentItem.id = index + 1;
-      currentItem.key = index + "";
-      index = index + 1;
-      return currentItem;
-    })
-    setItems(newItems);
-    setAccomplishment("");
-    update(ref(database, 'users/' + user.uid), {
-        data: items
-    });
-  }*/
-
   const deleteCard = id => {
     // console.log("Button pushed for card", id);
     let newItems = items.filter((currentItem) => {
@@ -120,95 +83,9 @@ function Bank(props) {
     });
   }
 
-  /*const editTag = value => {
-    let newTags = tags;
-    if (!tags.includes(value)) {
-      newTags.push(value)
-    } else {
-      let index = newTags.indexOf(value);
-      if (index > -1) {
-        newTags.splice(index, 1);
-      }
-    }
-    setTags(newTags);
-  }*/
-
   const entriesToShow = items.filter((currentItem) => {
     return (filter === "none" || currentItem.tags?.includes(filter));
   });
-
-  /*function entryForm() {
-   return (
-      <div>
-        <p>Bank Page</p>
-        <form>
-        <h4>What's something you're proud of?</h4>
-        <p><em>This only works if you are already logged in.</em></p>
-        <input
-          placeholder="Today I was able to..."
-          value={accomplishment}
-          onChange={(event) => {
-            setAccomplishment(event.target.value);
-          }}
-        />
-        <br></br>
-        <input
-          type="radio"
-          value="success"
-          name="status"
-          defaultChecked={status === "success"}
-          onChange={e => setStatus(e.currentTarget.value)}
-        /> Success
-        <br></br>
-        <input
-          type="radio"
-          value="question-unanswered"
-          name="status"
-          onChange={e => setStatus(e.currentTarget.value)}
-        /> Question (Unanswered)
-        <br></br>
-        <p><u>Tags</u></p>
-        <input
-          type="checkbox"
-          value="technical"
-          name="tag"
-          onChange={e => editTag(e.currentTarget.value)}
-        /> Technical
-        <br></br>
-        <input
-          type="checkbox"
-          value="soft skills"
-          name="tag"
-          onChange={e => editTag(e.currentTarget.value)}
-        /> Soft Skills
-        <br></br>
-        <br></br>
-        <button onClick={addNewAccomplishment}>Add accomplishment</button>
-        </form>
-        <br></br>
-        <h4>Want to filter by a specific tag?</h4>
-        <input
-          type="radio"
-          value="none"
-          name="filter"
-          defaultChecked={true}
-          onChange={e => setFilter(e.currentTarget.value)}
-        /> None
-        <input
-          type="radio"
-          value="technical"
-          name="filter"
-          onChange={e => setFilter(e.currentTarget.value)}
-        /> Technical
-        <input
-          type="radio"
-          value="soft skills"
-          name="filter"
-          onChange={e => setFilter(e.currentTarget.value)}
-        /> Soft Skills
-      </div>
-    )
-  }*/
 
   if (items.length > 0) {
     return (
@@ -239,12 +116,6 @@ function Bank(props) {
 
     )
   }
-
-  /*return (
-    <div>
-      <p>{text}</p>
-    </div>
-  );*/
 }
 
 export default Bank;
