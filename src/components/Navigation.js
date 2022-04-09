599cd97cdd6716a1f6e50321ded6321facc1a9f9
ZachGrande <zach.grande23@gmail.com>
2022-04-08 22:34:15 -0700

    Show leaf icon when not logged in

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,19 +1,17 @@
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword,
-  onAuthStateChanged, signOut, updateProfile } from 'firebase/auth';
-import { getDatabase, ref, set, update, onValue } from 'firebase/database';
-
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import app from '../config';
+import leafActive from '../assets/leaf-active.svg';
+import leafInactive from '../assets/leaf-inactive.svg';
 
 const auth = getAuth(app);
-const database = getDatabase(app);
 
 function Navigation() {
 
   const [user, setUser] = useState();
   const [initials, setInitials] = useState("");
+  const [profileButton, setProfileButton] = useState(leafInactive);
 
   onAuthStateChanged(auth, (currentUser) => {
     setUser(currentUser);
@@ -22,6 +20,14 @@ function Navigation() {
   useEffect(() => {
     setInitials(user?.displayName);
   }, [user])
+
+  const toggleProfileButton = () => {
+    if (profileButton == leafInactive) {
+      setProfileButton(leafActive);
+    } else {
+      setProfileButton(leafInactive);
+    }
+  }
 
   return(
     <nav>
@@ -64,7 +70,14 @@ function Navigation() {
               Sign In
             </Link>
           </li>
-          {initials ? <p>{initials}</p> : <p>Profile block</p>}
+          <li>
+            <Link to="/authentication">
+              {initials ? <p>{initials}</p> : <img src={profileButton}
+                                              onMouseOver={toggleProfileButton}
+                                              onMouseLeave={toggleProfileButton}
+                                              width="50"></img>}
+            </Link>
+          </li>
           {/* <p>Profile block</p> */}
         </ul>
       </div>
