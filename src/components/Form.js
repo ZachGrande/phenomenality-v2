cd8eb32fe89de8722f44628526af06450bf2b525
ttytiff <59585119+ttytiff@users.noreply.github.com>
2022-04-11 19:00:08 -0700

    add tag component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { ref, update } from 'firebase/database';
 import { map } from '@firebase/util';
+import { withContext as ReactTags } from "react-tag-input";
 
 function Form(props) {
 
@@ -14,6 +15,30 @@ function Form(props) {
   const [status, setStatus] = useState("success");
   const [tags, setTags] = useState([]);
 
+  const [searchTags, setSearchTags] = useState([]);
+  const KeyCodes = {
+    comma: 188,
+    enter: 13
+  };
+  const delimiters = [KeyCodes.comma, KeyCodes.enter];
+  const handleDelete = i => {
+    setSearchTags(searchTags.filter((tag, index) => index !== i));
+  };
+  const handleAddition = (tag) => {
+    setSearchTags([...searchTags, tag]);
+  };
+  const handleTagClick = index => {
+    console.log('The tag at index ' + index + ' was clicked');
+  };
+  const onClearAll = () => {
+    setSearchTags([]);
+  };
+  const onTagUpdate = (i, newTag) => {
+    const updatedSearchTags = searchTags.slice();
+    updatedSearchTags.splice(i, 1, newTag);
+    setSearchTags(updatedSearchTags);
+  };
+
   const addNewAccomplishment = async (event) => { 
     event.preventDefault();
     let thisAccomplishment = {
@@ -24,6 +49,9 @@ function Form(props) {
       status: status,
       tags: tags
     }
+
+    console.log(tags) //tags spits out array based on order on selection of tag
+    
     let newItems = items.push(thisAccomplishment);
     newItems = map((currentItem, index = 0, newItems) => {
       currentItem.id = index + 1;
@@ -31,6 +59,7 @@ function Form(props) {
       index = index + 1;
       return currentItem;
     })
+
     setItems(newItems);
     setAccomplishment("");
     update(ref(database, 'users/' + user.uid), {
@@ -119,6 +148,19 @@ function Form(props) {
          name="filter"
          onChange={e => setFilter(e.currentTarget.value)}
        /> Soft Skills
+       <br />
+       <div>
+       <ReactTags
+          handleDelete={handleDelete}
+          handleAddition={handleAddition}
+          delimiters={delimiters}
+          handleTagClick={handleTagClick}
+          onClearAll={onClearAll}
+          onTagUpdate={onTagUpdate}
+          placeholder="Search..."
+          tags={searchTags}
+        />
+        </div>
      </div>
    )
  }
