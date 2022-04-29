78ee333d2f9585b7ab7adf2ba2ec8a236bdc3066
ZachGrande <zach.grande23@gmail.com>
2022-04-29 11:02:19 -0700

    Fix TagButton indentation

--- src/components/TagButton.js
+++ src/components/TagButton.js
@@ -2,49 +2,48 @@ import React from 'react';
 import '../css/TagButton.css'
 
 function TagButton(props){
-    let thisItem = props.item;
-    let activeTags = props.activeTags;
+  let thisItem = props.item;
+  let activeTags = props.activeTags;
 
-    // useEffect(() => {
+  // useEffect(() => {
 
-    // }, [activeTags])
+  // }, [activeTags])
 
-    const getClassName = () => {
-      let className = "tag-test " + thisItem.class;
-      if (activeTags.includes(thisItem.description)) {
-        className = className + " active";
-      } else {
-        className = className + " inactive";
-      }
-      console.log(className);
-      return className;
+  const getClassName = () => {
+    let className = "tag-test " + thisItem.class;
+    if (activeTags.includes(thisItem.description)) {
+      className = className + " active";
+    } else {
+      className = className + " inactive";
     }
+    console.log(className);
+    return className;
+  }
     
-    function renderTagButton(){
-        /*const handleEditTag = (event) => {
-            props.editTag(thisItem);
-        }*/
+  function renderTagButton(){
+    /*const handleEditTag = (event) => {
+        props.editTag(thisItem);
+    }*/
 
-      const handleClick = (event) => {
-        console.log("Clicked", thisItem.class);
-        props.toggleTag(thisItem.description);
-      }
-
-        // let idName = thisItem.replace(/\s+/g, '');
-
-        return(
-            
-            <div>
-                <input
-                // id={idName}
-                className={getClassName()}
-                type='button'
-                value={thisItem.description}
-                onClick={handleClick}
-                />
-            </div>
-        )
+    const handleClick = (event) => {
+      console.log("Clicked", thisItem.class);
+      props.toggleTag(thisItem.description);
     }
+
+    // let idName = thisItem.replace(/\s+/g, '');
+
+    return (
+      <div>
+        <input
+        // id={idName}
+        className={getClassName()}
+        type='button'
+        value={thisItem.description}
+        onClick={handleClick}
+        />
+      </div>
+    )
+  }
 
     return (
         <div>
@@ -54,21 +53,21 @@ function TagButton(props){
 }
 
 function TagButtonList(props){
-    let items = props.items;
+  let items = props.items;
 
-    let tagBtnComponenets = items?.map((currentItem, index) => {
-        return <TagButton key={index} item={currentItem}
-        activeTags={props.activeTags}
-        toggleTag={props.toggleTag}
-        color={props.color[index]}
-        />
-    })
+  let tagBtnComponenets = items?.map((currentItem, index) => {
+      return <TagButton key={index} item={currentItem}
+      activeTags={props.activeTags}
+      toggleTag={props.toggleTag}
+      color={props.color[index]}
+      />
+  })
 
-    return (
-        <div className='tag-btn-list'>
-            {tagBtnComponenets}
-        </div>
-    )
+  return (
+      <div className='tag-btn-list'>
+          {tagBtnComponenets}
+      </div>
+  )
 }
 
 export default TagButtonList;
