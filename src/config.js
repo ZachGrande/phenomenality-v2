6abc2dc07eda101fbc22f3da69013db64444b493
Zach Grande <zach.grande23@gmail.com>
2024-04-27 11:47:20 -0700

    Configure new firebase app

--- src/config.js
+++ src/config.js
@@ -1,18 +1,19 @@
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const config = {
-  apiKey: "AIzaSyA_6VtPAZLUSF6828G0peEoERJdDDZ8fy0",
-  authDomain: "phenomenality-bdf65.firebaseapp.com",
-  databaseURL: "https://phenomenality-bdf65-default-rtdb.firebaseio.com",
-  projectId: "phenomenality-bdf65",
-  storageBucket: "phenomenality-bdf65.appspot.com",
-  messagingSenderId: "47065258935",
-  appId: "1:47065258935:web:e1c40865d25e46a80ce632",
-  measurementId: "G-HP9BTNZ836"
+const firebaseConfig = {
+  apiKey: "AIzaSyC86Eh6E-qXceGRFysMiVGh1lYHU33uFIo",
+  authDomain: "phenomenality-v2.firebaseapp.com",
+  projectId: "phenomenality-v2",
+  storageBucket: "phenomenality-v2.appspot.com",
+  messagingSenderId: "574507999",
+  appId: "1:574507999:web:e4d2dccf5ee809b4d7d26a",
+  measurementId: "G-30RDWK6ZSG"
 };
 
-const app = initializeApp(config);
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export default app;
