e16ec328f444b8dc5a9f5789ec505229e5c1502f
Zach Grande <zach.grande23@gmail.com>
2024-05-27 12:38:33 -0700

    Extract routes into separate file

--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,7 @@ import React from 'react';
 import '../styles/App.sass';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
-import Landing from './Landing.js';
-import Bank from './Bank.js';
-import Questions from './Questions.js';
-import Quiz from './Quiz.js';
-import ImposterInfo from './ImposterInfo.js';
-import About from './About.js';
-import Authentication from './Authentication.js';
-import AddAccomplishment from './AddAccomplishment.js';
-import AddEncouragement from './AddEncouragement.js';
-import Perfectionist from './types/Perfectionist.js';
-import Superhuman from './types/Superhuman.js';
-import Genius from './types/Genius.js';
-import Soloist from './types/Soloist.js';
-import Expert from './types/Expert.js';
-import HomeLayout from './HomeLayout.js';
+import SiteRouter from './SiteRouter';
 
 export default class App extends React.Component {
 
@@ -29,23 +15,8 @@ export default class App extends React.Component {
           <title>Phenomenality</title>
         </Helmet>
         <Router>
-          <Routes className="content-wrap">
-            <Route element={<HomeLayout />}>
-              <Route index path="/" element={<Landing />} />
-              <Route exact={true} path="/accomplishments" element={<AddAccomplishment />} />
-              <Route exact={true} path="/accomplishments-complete" element={<AddEncouragement />} />
-              <Route exact={true} path="/bank" element={<Bank />} />
-              <Route exact={true} path="/questions" element={<Questions />} />
-              <Route exact={true} path="/quiz" element={<Quiz />} />
-              <Route exact={true} path="/more-info" element={<ImposterInfo />} />
-              <Route exact={true} path="/about" element={<About />} />
-              <Route exact={true} path="/authentication" element={<Authentication />} />
-              <Route exact={true} path="/type-1" element={<Perfectionist />} />
-              <Route exact={true} path="/type-2" element={<Superhuman />} />
-              <Route exact={true} path="/type-3" element={<Genius />} />
-              <Route exact={true} path="/type-4" element={<Soloist />} />
-              <Route exact={true} path="/type-5" element={<Expert />} />
-            </Route>
+          <Routes>
+            <Route path="/*" element={<SiteRouter />} />
           </Routes>
         </Router>
         <footer>
