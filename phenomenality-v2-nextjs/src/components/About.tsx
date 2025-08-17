81e9b80e69942389f79bf01c1b009e0ac37e985a
Zach Grande <zach.grande23@gmail.com>
2025-08-17 16:15:50 -0700

    lint: About page

--- phenomenality-v2-nextjs/src/components/About.tsx
+++ phenomenality-v2-nextjs/src/components/About.tsx
@@ -1,6 +1,6 @@
 import './About.module.sass';
 
-function About() {
+export default function About() {
   const ZachGrande = './images/ZachGrande.png';
   const EliseAdams = './images/EliseAdams.jpg';
   const ValerieTse = './images/ValerieTse.jpg';
@@ -8,48 +8,50 @@ function About() {
   const RachelKinkley = './images/RachelKinkley.jpeg';
   const jackjack = './images/jack-jack.svg';
 
-  return(
+  return (
     <div className="about">
-        <h1>the team</h1>
+      <h1>the team</h1>
 
-        <div className="member-group">
-          <div className="member">
-            <img src={EliseAdams} alt="Elise"/>
-            <p className="member-name">elise adams</p>
-            <p className="member-role">project manager/research</p>
-          </div>
-          <div className="member">
-            <img src={ZachGrande} alt="Zach"/>
-            <p className="member-name">zach grande</p>
-            <p className="member-role">full-stack development</p>
-          </div>
-          <div className="member">
-            <img src={RachelKinkley} alt="Rachel"/>
-            <p className="member-name">rachel kinkley</p>
-            <p className="member-role">front-end/research</p>
-          </div>
-          <div className="member">
-            <img src={TiffanyTse} alt="Tiffany"/>
-            <p className="member-name">tiffany tse</p>
-            <p className="member-role">front-end/ui</p>
-          </div>
-          <div className="member">
-            <img src={ValerieTse} alt="Valerie"/>
-            <p className="member-name">valerie tse</p>
-            <p className="member-role">program manager/ux</p>
-          </div>
+      <div className="member-group">
+        <div className="member">
+          <img src={EliseAdams} alt="Elise" />
+          <p className="member-name">elise adams</p>
+          <p className="member-role">project manager/research</p>
         </div>
+        <div className="member">
+          <img src={ZachGrande} alt="Zach" />
+          <p className="member-name">zach grande</p>
+          <p className="member-role">full-stack development</p>
+        </div>
+        <div className="member">
+          <img src={RachelKinkley} alt="Rachel" />
+          <p className="member-name">rachel kinkley</p>
+          <p className="member-role">front-end/research</p>
+        </div>
+        <div className="member">
+          <img src={TiffanyTse} alt="Tiffany" />
+          <p className="member-name">tiffany tse</p>
+          <p className="member-role">front-end/ui</p>
+        </div>
+        <div className="member">
+          <img src={ValerieTse} alt="Valerie" />
+          <p className="member-name">valerie tse</p>
+          <p className="member-role">program manager/ux</p>
+        </div>
+      </div>
 
-        <div className='thanks'>
-          <p>with many thanks to jeremy zaretzky, emily porter, laura schildkraut, mina tari, and milla titova</p>
-          <h6>NOTICE UPDATE: beginning 5/26 the development of “phenomenality” has shut down.
-</h6>
-          <div className="jackjack-container">
+      <div className="thanks">
+        <p>
+          with many thanks to jeremy zaretzky, emily porter, laura schildkraut,
+          mina tari, and milla titova
+        </p>
+        <h6>
+          NOTICE UPDATE: beginning 5/26 the development of “phenomenality” has shut down.
+        </h6>
+        <div className="jackjack-container">
           <img className="jackjack" src={jackjack} alt="logo" />
-          </div>
         </div>
+      </div>
     </div>
-    )
-  }
-  
-  export default About;
+  );
+}
