bf67328204b05642b7edcca0354f60a111daa98b
Zach Grande <zach.grande23@gmail.com>
2025-08-17 09:28:59 -0700

    lint: navigation component

--- phenomenality-v2-nextjs/src/components/Navigation.tsx
+++ phenomenality-v2-nextjs/src/components/Navigation.tsx
@@ -16,20 +16,18 @@ const downCarrot = 'assets/down-carrot.svg';
 const auth = getAuth(app);
 
 function Navigation() {
-
   const [user, setUser] = useState();
-  const [initials, setInitials] = useState("");
+  const [initials, setInitials] = useState('');
   const [profileButton, setProfileButton] = useState(leafInactive);
   const [showDropDownMenu, setShowDropDownMenu] = useState(false);
 
-
   onAuthStateChanged(auth, (currentUser) => {
     setUser(currentUser);
-  })
+  });
 
   useEffect(() => {
     setInitials(user?.displayName);
-  }, [user])
+  }, [user]);
 
   const toggleProfileButton = () => {
     if (profileButton === leafInactive) {
@@ -37,15 +35,15 @@ function Navigation() {
     } else {
       setProfileButton(leafInactive);
     }
-  }
+  };
 
-function toggleDropDown() {
-  setShowDropDownMenu(!showDropDownMenu);
-}
+  function toggleDropDown() {
+    setShowDropDownMenu(!showDropDownMenu);
+  }
 
   return (
     <nav>
-      <div className={`${styles.navigation} navigation w-100 p-3`}>
+      <div className={clsx(styles.navigation, 'w-100 p-3')}>
         <ul className="mb-0 px-0">
           <Link className={clsx(styles.keepStrengthen, styles['navbar-link'], styles['home-font'])} href="/">
             phenomenality | strengthen your mentality
@@ -53,104 +51,117 @@ function toggleDropDown() {
           <Link className={clsx(styles.dropStrengthen, styles['navbar-link'], styles['home-font'])} href="/">
             phenomenality
           </Link>
-          {showDropDownMenu ?
-            <div className={clsx(styles.dropDown, styles.dropDownContainer)} id="popupForm">
-              <ul className="px-4">
-                <li className={clsx(styles.navvy, "d-inline")}>
-                  <Link className={clsx(styles["navbar-link"], styles['link-font'])} href="/accomplishments">
+          {showDropDownMenu
+            ? (
+              <div className={clsx(styles.dropDown, styles.dropDownContainer)} id="popupForm">
+                <ul className="px-4">
+                  <li className={clsx(styles.navvy, 'd-inline')}>
+                    <Link className={clsx(styles['navbar-link'], styles['link-font'])} href="/accomplishments">
+                      accomplishments
+                    </Link>
+                    <br />
+                    <br />
+                  </li>
+                  <li className={clsx(styles.navvy, 'd-inline')}>
+                    <Link className={clsx(styles['navbar-link'], styles['link-font'])} href="/bank">
+                      your bank
+                    </Link>
+                    <br />
+                    <br />
+                  </li>
+                  <li className={clsx(styles.navvy, 'd-inline')}>
+                    <Link className={clsx(styles['navbar-link'], styles['link-font'])} href="quiz">
+                      quiz
+                    </Link>
+                    <br />
+                    <br />
+                  </li>
+                  <li className={clsx(styles.navvy, 'd-inline')}>
+                    <Link className={clsx(styles['navbar-link'], styles['link-font'])} href="more-info">
+                      imposter phenomenon
+                    </Link>
+                    <br />
+                  </li>
+                  <br />
+                  <br />
+                  <li className={clsx(styles.dropdownInitials, 'd-inline')}>
+                    <Link className={clsx(styles['navbar-link'], styles['link-font'], styles.dropdownInitials)} href="/authentication">
+                      {initials
+                        ? (
+                          <div className={styles.initials}>
+                            {initials}
+                          </div>
+                        )
+                        : (
+                          <img
+                            src={profileButton}
+                            onMouseOver={toggleProfileButton}
+                            onMouseLeave={toggleProfileButton}
+                            width="50"
+                            alt="profile"
+                          />
+                        )}
+                    </Link>
+                  </li>
+                </ul>
+                <div className={styles['popup-btn-center']}>
+                  {/* <button type="button" className="btn cancel" onClick={closeDropDownMenu}>close</button> */}
+                </div>
+              </div>
+            )
+            : (
+              <div className={clsx(styles['nav-left'])}>
+                <li className={clsx(styles.navvy, 'd-inline', 'mx-2')}>
+                  <Link className={clsx(styles['navbar-link'], styles['link-font'])} href="/accomplishments">
                     accomplishments
                   </Link>
-                  <br></br>
-                  <br></br>
                 </li>
-                <li className={clsx(styles.navvy, "d-inline")}>
-                  <Link className={clsx(styles["navbar-link"], styles["link-font"])} href="/bank">
+                <li className={clsx(styles.navvy, 'd-inline', 'mx-2')}>
+                  <Link className={clsx(styles['navbar-link'], styles['link-font'])} href="/bank">
                     your bank
                   </Link>
-                  <br></br>
-                  <br></br>
                 </li>
-                <li className={clsx(styles.navvy, "d-inline")}>
-                  <Link className={clsx(styles["navbar-link"], styles["link-font"])} href="quiz">
+                <li className={clsx(styles.navvy, 'd-inline', 'mx-2')}>
+                  <Link className={clsx(styles['navbar-link'], styles['link-font'])} href="quiz">
                     quiz
                   </Link>
-                  <br></br>
-                  <br></br>
                 </li>
-                <li className={clsx(styles.navvy, "d-inline")}>
-                  <Link className={clsx(styles["navbar-link"], styles["link-font"])} href="more-info">
+                <li className={clsx(styles.navvy, 'd-inline', 'mx-2')}>
+                  <Link className={clsx(styles['navbar-link'], styles['link-font'])} href="more-info">
                     imposter phenomenon
                   </Link>
-                  <br></br>
                 </li>
-                <br></br>
-                <br></br>
-                <li className={clsx(styles.dropdownInitials, "d-inline")}>
-                  <Link className={clsx(styles["navbar-link"], styles["link-font"], styles.dropdownInitials)} href="/authentication">
-                  {initials ?
-                    <div className={styles.initials}>
-                      {initials}
-                    </div>
-                  :
-                    <img src={profileButton}
-                    onMouseOver={toggleProfileButton}
-                    onMouseLeave={toggleProfileButton}
-                    width="50"
-                    alt="profile"></img>}
+                <li className="d-inline mx-2">
+                  <Link className={clsx(styles['navbar-link'], styles['link-font'])} href="/authentication">
+                    {initials
+                      ? (
+                        <div className="initials">
+                          {initials}
+                        </div>
+                      )
+                      : (
+                        <img
+                          src={profileButton}
+                          onMouseOver={toggleProfileButton}
+                          onMouseLeave={toggleProfileButton}
+                          width="46"
+                          alt="profile"
+                        />
+                      )}
                   </Link>
                 </li>
-              </ul>
-              <div className={styles["popup-btn-center"]}>
-                {/* <button type="button" className="btn cancel" onClick={closeDropDownMenu}>close</button> */}
               </div>
+            ) }
+          <button className={styles['carrot-menu']} onClick={toggleDropDown}>
+            <div className={styles.menu}>
+              <p className={clsx(styles['link-font'], 'mb-0 d-inline-block align-top')}>menu</p>
+              <img className={clsx(styles['carrot-icon'], 'd-inline-block', (!showDropDownMenu ? styles.rotated : ''))} src={downCarrot} alt="Bank" width="12%" height="12%" />
             </div>
-          : 
-            <div className={clsx(styles["nav-left"])}>
-              <li className={clsx(styles.navvy, "d-inline", "mx-2")}>
-                <Link className={clsx(styles["navbar-link"], styles["link-font"])} href="/accomplishments">
-                  accomplishments
-                </Link>
-              </li>
-              <li className={clsx(styles.navvy, "d-inline", "mx-2")}>
-                <Link className={clsx(styles["navbar-link"], styles["link-font"])} href="/bank">
-                  your bank
-                </Link>
-              </li>
-              <li className={clsx(styles.navvy, "d-inline", "mx-2")}>
-                <Link className={clsx(styles["navbar-link"], styles["link-font"])} href="quiz">
-                  quiz
-                </Link>
-              </li>
-              <li className={clsx(styles.navvy, "d-inline", "mx-2")}>
-                <Link className={clsx(styles["navbar-link"], styles["link-font"])} href="more-info">
-                  imposter phenomenon
-                </Link>
-              </li>
-              <li className="d-inline mx-2">
-                <Link className={clsx(styles["navbar-link"], styles["link-font"])} href="/authentication">
-                  {initials ?
-                    <div className="initials">
-                      {initials}
-                    </div>
-                  :
-                    <img src={profileButton}
-                    onMouseOver={toggleProfileButton}
-                    onMouseLeave={toggleProfileButton}
-                    width="46"
-                    alt="profile"></img>}
-                </Link>
-              </li>
-            </div> }
-            <button className={styles['carrot-menu']} onClick={toggleDropDown}>
-              <div className={styles.menu}>
-                <p className={clsx(styles["link-font"], "mb-0 d-inline-block align-top")}>menu</p>
-                <img className={clsx(styles["carrot-icon"], "d-inline-block", (!showDropDownMenu ? styles.rotated : ''))} src={downCarrot} alt="Bank" width="12%" height="12%"/>
-              </div>
-            </button>
+          </button>
         </ul>
       </div>
     </nav>
-  )
+  );
 }
 
 export default Navigation;
